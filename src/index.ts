1538c3753eaca96d17f7c8cb8f9a5ebbe39603ce
tshion <tenta_shion@yahoo.co.jp>
2024-05-11 13:25:13 +0900

    Git コマンド構築方法のブラッシュアップ

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,40 @@
 import core from '@actions/core';
 import { exec } from '@actions/exec';
+import { GitCommand, GitCommandBuilder } from './git-command';
 import { GitUser, GitUserUtil } from './git-user.type';
 
 +async function () {
   try {
-    const inputPath = core.getInput('path');
-
     const user = GitUserUtil.parseOrNull(core.getInput('user', { required: true }));
     if (!user) {
       throw Error(`Please set 'user' to either ${GitUserUtil.toTextForSelection()}.`);
     }
 
-    let email = '';
-    let name = '';
+    const builder = new GitCommandBuilder(
+      core.getBooleanInput('global'),
+      core.getInput('path'),
+    );
+    let gitCommand: GitCommand;
     switch (user) {
       case GitUser.ACTIONS_USER:
-        email = '65916846+actions-user@users.noreply.github.com';
-        name = 'actions-user';
+        gitCommand = builder.forActionsUser();
         break;
       case GitUser.GITHUB_ACTIONS:
-        email = '41898282+github-actions[bot]@users.noreply.github.com';
-        name = 'github-actions[bot]';
+        gitCommand = builder.forGitHubActions();
         break;
       case GitUser.LATEST_COMMIT:
-        await exec(
-          `git --no-pager log --format=format:'%ae' -n 1`,
-          undefined,
-          {
-            cwd: inputPath,
-            listeners: {
-              stdout(data) {
-                email = data.toString();
-              },
-            }
-          },
-        );
-        await exec(
-          `git --no-pager log --format=format:'%an' -n 1`,
-          undefined,
-          {
-            cwd: inputPath,
-            listeners: {
-              stdout(data) {
-                name = data.toString();
-              },
-            }
-          },
-        );
+        gitCommand = builder.forLatestCommit();
         break;
       case GitUser.SPECIFIC:
-        email = core.getInput('email');
-        name = core.getInput('name');
+        gitCommand = builder.forSpecific(
+          core.getInput('email'),
+          core.getInput('name'),
+        );
         break;
     }
-    if (!email || !name) {
-      throw Error('User not found');
-    }
 
-    const gitFlag = core.getBooleanInput('global') ? '--global' : '--local';
-    await exec(`git config ${gitFlag} user.email ${email}`, [], { cwd: inputPath });
-    await exec(`git config ${gitFlag} user.name ${name}`, [], { cwd: inputPath });
+    await exec(gitCommand.commandUserEmail, undefined, gitCommand.options);
+    await exec(gitCommand.commandUserName, undefined, gitCommand.options);
   } catch (error: unknown) {
     if (error instanceof Error) {
       core.setFailed(error.message);
