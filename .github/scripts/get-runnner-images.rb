8c1dd94a3f4eb4e8f29a1768a80171ef69060fda
tshion <tenta_shion@yahoo.co.jp>
2024-06-29 14:05:12 +0900

    Ruby 実装の調整

--- .github/scripts/get-runnner-images.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'json'
-
-images = { os: ["ubuntu-latest", "windows-latest", "macos-latest"] }
-File.open("config.json", "w") { |file|
-    file.write(images.to_json)
-}
